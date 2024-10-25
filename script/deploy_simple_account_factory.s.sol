ffcc1d2d91fb4c40551502b0763a76a835da0757
Johnson Chen <chnejohnson@gmail.com>
2024-10-25 16:49:13 +0800

    add deploy factory script

---
+++ script/deploy_simple_account_factory.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {SimpleAccountFactory} from "@infinitism/samples/SimpleAccountFactory.sol";
+import {IEntryPoint} from "@infinitism/interfaces/IEntryPoint.sol";
+
+contract DeploySimpleAccountFactoryScript is Script {
+    SimpleAccountFactory public factory;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        address entrypointV07 = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
+        factory = new SimpleAccountFactory(IEntryPoint(entrypointV07));
+
+        vm.stopBroadcast();
+    }
+}
