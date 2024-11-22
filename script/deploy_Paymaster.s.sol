3d6123e5152e657014c6fb2a4735904beca04d18
Johnson Chen <chnejohnson@gmail.com>
2024-11-22 11:08:47 +0800

    add notes about deposit to entrypoint for paymaster

--- script/deploy_Paymaster.s.sol
+++ script/deploy_Paymaster.s.sol
@@ -7,6 +7,21 @@ import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint
 
 // forge script --rpc-url $sepolia script/deploy_Paymaster.s.sol --account dev --verify --broadcast
 
+/* @note deposit to entrypoint for paymaster
+cast send --account dev  --rpc-url $sepolia \
+    0x0000000071727De22E5E9d8BAf0edAc6f37da032 \
+    "depositTo(address account)" \
+    0xA2E1944eD3294f0202a063cc971ECe09cbd02e43 \
+    --value 0.1ether
+*/
+
+/* @note check balance of paymaster
+cast call --rpc-url $sepolia \
+    0x0000000071727De22E5E9d8BAf0edAc6f37da032 \
+    "balanceOf(address account)" \
+    0xA2E1944eD3294f0202a063cc971ECe09cbd02e43
+*/
+
 contract DeployPaymasterScript is Script {
     Paymaster public factory;
 
