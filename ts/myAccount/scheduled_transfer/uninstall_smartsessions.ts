01339b95e9b9c3828c4056dea2de1cf0ca483676
Johnson Chen <chnejohnson@gmail.com>
2024-11-26 15:16:31 +0800

    bug: failed to uninstall smartsessions

---
+++ ts/myAccount/scheduled_transfer/uninstall_smartsessions.ts
@@ -0,0 +1,35 @@
+import { Interface } from 'ethers'
+import { WalletService, type CallsResult } from '../../WalletService'
+import { MY_ACCOUNT_ADDRESS, SMART_SESSION_ADDRESS } from './utils'
+
+const walletService = new WalletService({ supportPaymaster: true })
+
+const call = {
+	to: MY_ACCOUNT_ADDRESS,
+	data: new Interface([
+		'function uninstallModule(uint256 moduleTypeId, address module, bytes calldata deInitData)',
+	]).encodeFunctionData('uninstallModule', [1, SMART_SESSION_ADDRESS, '0x']),
+	value: '0x0',
+}
+
+const identifier = await walletService.sendCalls({
+	version: '1',
+	from: MY_ACCOUNT_ADDRESS,
+	calls: [call],
+})
+
+let result: CallsResult | null = null
+
+while (!result || result.status === 'PENDING') {
+	result = await walletService.getCallStatus(identifier)
+
+	if (!result || result.status === 'PENDING') {
+		await new Promise(resolve => setTimeout(resolve, 1000))
+	}
+}
+
+if (result.status === 'CONFIRMED' && result?.receipts) {
+	result.receipts.forEach(receipt => {
+		console.log('txHash', receipt.transactionHash)
+	})
+}
