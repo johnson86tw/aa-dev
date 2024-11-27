6e08132b5eddce60d040b8ca9db5c02206950594
Johnson Chen <chnejohnson@gmail.com>
2024-11-27 12:52:44 +0800

    fix: uninstall smartsessions

--- ts/myAccount/scheduled_transfer/uninstall_smartsessions.ts
+++ ts/myAccount/scheduled_transfer/uninstall_smartsessions.ts
@@ -1,6 +1,43 @@
-import { Interface } from 'ethers'
+import { AbiCoder, Contract, Interface, JsonRpcProvider } from 'ethers'
 import { WalletService, type CallsResult } from '../../WalletService'
-import { MY_ACCOUNT_ADDRESS, SMART_SESSION_ADDRESS } from './utils'
+import { MY_ACCOUNT_ADDRESS, padLeft, SMART_SESSION_ADDRESS } from './utils'
+
+if (!process.env.sepolia) {
+	throw new Error('Missing .env')
+}
+const RPC_URL = process.env.sepolia
+
+const provider = new JsonRpcProvider(RPC_URL)
+
+// ================================ build deInitData ==========================================
+
+const myAccount = new Contract(
+	MY_ACCOUNT_ADDRESS,
+	[
+		'function getValidatorsPaginated(address cursor, uint256 size) external view returns (address[] memory array, address next)',
+	],
+	provider,
+)
+
+const validators = await myAccount.getValidatorsPaginated(padLeft('0x1', 20), 5)
+const prev = findPrevious(validators.array, SMART_SESSION_ADDRESS)
+
+const deInitData = new AbiCoder().encode(['address', 'bytes'], [prev, '0x'])
+
+function findPrevious(array: string[], entry: string): string {
+	for (let i = 0; i < array.length; i++) {
+		if (array[i].toLowerCase() === entry.toLowerCase()) {
+			if (i === 0) {
+				return padLeft('0x1', 20)
+			} else {
+				return array[i - 1]
+			}
+		}
+	}
+	throw new Error('Entry not found in array')
+}
+
+// ================================ end build deInitData ==========================================
 
 const walletService = new WalletService({ supportPaymaster: true })
 
@@ -8,7 +45,7 @@ const call = {
 	to: MY_ACCOUNT_ADDRESS,
 	data: new Interface([
 		'function uninstallModule(uint256 moduleTypeId, address module, bytes calldata deInitData)',
-	]).encodeFunctionData('uninstallModule', [1, SMART_SESSION_ADDRESS, '0x']),
+	]).encodeFunctionData('uninstallModule', [1, SMART_SESSION_ADDRESS, deInitData]),
 	value: '0x0',
 }
 
