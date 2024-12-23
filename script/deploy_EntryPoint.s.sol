f78e04d4efaa09a33f2a0aeae70befb24bdd6bdd
Johnson Chen <chnejohnson@gmail.com>
2024-12-23 16:36:26 +0800

    update deploy_EntryPoint.s.sol

--- script/deploy_EntryPoint.s.sol
+++ script/deploy_EntryPoint.s.sol
@@ -6,8 +6,9 @@ import {EntryPoint} from "@account-abstraction/contracts/core/EntryPoint.sol";
 
 contract DeployEntryPointScript is Script {
     function run() public {
-        bytes32 salt = 0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3;
-        EntryPoint entryPoint = new EntryPoint{salt: salt}();
+        vm.startBroadcast();
+        EntryPoint entryPoint =
+            new EntryPoint{salt: 0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3}();
         console.log("EntryPoint deployed at:", address(entryPoint));
     }
 }
